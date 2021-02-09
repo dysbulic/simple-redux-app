ebec5c596b2ef53d2315394483cf8ad48f036596
Will Holcomb <dys@dhappy.org>
2021-02-09 01:29:36 -0500

    🎪 npx create-react-app basic-redux

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { connect, Provider } from 'react-redux'
+import { createSlice, configureStore } from '@reduxjs/toolkit'
+
+const pageSlice = createSlice({
+  name: 'page',
+  initialState: 1,
+  reducers: {
+    nextPage: state => state + 1,
+    previousPage: state => Math.min(1, state - 1)
+  }
+})
+
+const store = configureStore({
+  reducer: pageSlice.reducer
+})
+
+const ShowImpl = ({ page }) => {
+  return <h1>{page}</h1>
+}
+
+const Show = connect(page => ({ page }))(ShowImpl)
+
+const Next = () => (
+  <button onClick={pageSlice.actions.nextPage}>+</button>
+)
+
+export default () => (
+  <Provider store={store}>
+    <Show/>
+    <Next/>
+  </Provider>
+)
